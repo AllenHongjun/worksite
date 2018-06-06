08ae21bf997b34e0da1d8cf550433f18205314c9
allenHong <652971723@163.com>
2018-06-06 15:59:03 +0800

    第一次使用
    初始化

---
+++ 企业官网/js/banner.js
@@ -0,0 +1,15 @@
+//banner
+$(".banner").slide({
+    titCell: ".hd ul",
+    mainCell: ".bd ul",
+    effect: "fold",
+    autoPlay: true,
+    autoPage: true,
+    trigger: "click",
+    startFun: function(i) {
+        var curLi = jQuery(".banner .bd li").eq(i);
+        if ( !! curLi.attr("_src")) {
+            curLi.css("background-image", curLi.attr("_src")).removeAttr("_src")
+        }
+    }
+});
